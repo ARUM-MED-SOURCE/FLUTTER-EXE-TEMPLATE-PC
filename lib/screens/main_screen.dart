9424c76f0ab25f1df330532275264a8c42e16ad1
heosangbeom <luv_3_u@naver.com>
2025-03-31 22:03:57 +0900

    fix: modify import

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_exe/components/main/MainComponent.dart';
+import 'package:flutter_exe/components/main/consent/ConsentSearch.dart';
 import 'package:flutter_exe/components/navigation/Topbar.dart';
 import 'package:flutter_exe/layout/default_layout.dart';
-import 'package:flutter_exe/components/main/MainComponent.dart';
-
-import 'package:flutter_exe/components/main/consent/ConsentSearch.dart';
 import 'package:flutter_exe/model/prescription_consent_data.dart';
 import 'package:flutter_exe/utils/dummy_data.dart';
 
