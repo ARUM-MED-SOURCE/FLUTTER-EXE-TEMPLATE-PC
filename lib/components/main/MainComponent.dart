574061b4c13695646f0db6c58735b3bdc5c619ed
heosangbeom <luv_3_u@naver.com>
2025-03-31 22:03:57 +0900

    fix: modify import

--- lib/components/main/MainComponent.dart
+++ lib/components/main/MainComponent.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_exe/components/main/header/MainHeader.dart';
 import 'package:flutter_exe/constants/colors.dart';
 
-import 'package:flutter_exe/components/main/MainHeader.dart';
 import 'package:flutter_exe/components/main/MainBody.dart';
 
 class MainComponent extends StatelessWidget {
